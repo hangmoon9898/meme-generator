58dd98d69de4df264fcf15c82622f8d55ac8a3da
hangmoon98 <lethithuyhang@borderless.ws>
2020-12-06 23:17:20 +0200

    Format code for better readability

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import rootReducer from './reducers';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
 
-import {fetchMemes} from './actions';
+import { fetchMemes } from "./actions";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
-store.subscribe(()=> console.log('store', store.getState()));
+store.subscribe(() => console.log("store", store.getState()));
 store.dispatch(fetchMemes());
 ReactDOM.render(
-<Provider store ={store}>
-<App/>
-</Provider>
-, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
